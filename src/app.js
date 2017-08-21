341f45d5145c77bbb88efb1cf3c30e8e8026bdba
realsumit <sumitsarinofficial@gmail.com>
2017-08-21 17:27:08 +0530

    initial app structure using socket.io added. Will run on localhost:3001

---
+++ src/app.js
@@ -0,0 +1,26 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get ("/",function (req, res){
+  res.sendFile('/Users/User/irc/src/html/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3001, function(){
+  console.log('listening on 3001');
+});
+
+
